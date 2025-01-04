bf82962e61a5eff122d3b2b838fbfd889e7705ca
Al-Mamun <mamun@leapinglogic.com>
2025-01-04 12:31:32 +0600

    Super update

--- src/class-schedules.js
+++ src/class-schedules.js
@@ -115,9 +115,18 @@ class Schedules {
       // data.push({
       //     "id": 6,
       //     "type": 1,
-      //     "title": "Morning",
-      //     "start_time": "09:00",
-      //     "end_time": "10:00",
+      //     "title": " puch test",
+      //     "start_time": "12:30",
+      //     "end_time": "12:32",
+      //     "classes": "[{\"class_name\":\"Play\",\"class_short\":\"play\",\"isActive\":true},{\"class_name\":\"Nursery\",\"class_short\":\"nursery\",\"isActive\":true},{\"class_name\":\"KG\",\"class_short\":\"kg\",\"isActive\":true},{\"class_name\":\"One/Saffe Awal\",\"class_short\":\"one\",\"isActive\":true},{\"class_name\":\"Two/Saffe Sani\",\"class_short\":\"two\",\"isActive\":true},{\"class_name\":\"Three/Saffe Sales\",\"class_short\":\"three\",\"isActive\":true},{\"class_name\":\"Four/Saffe Rabe\",\"class_short\":\"four\",\"isActive\":true},{\"class_name\":\"Ibtedaiyah\",\"class_short\":\"five\",\"isActive\":true},{\"class_name\":\"Mutawassitah Awal / Mizan\",\"class_short\":\"mizan\",\"isActive\":true},{\"class_name\":\"Mutawassitah Sani / Nahbemir\",\"class_short\":\"nahbemir\",\"isActive\":true},{\"class_name\":\"Mutawassitah Sales\",\"class_short\":\"kuduri\",\"isActive\":true},{\"class_name\":\"Sanabiya Awal/Shorhebekaya\",\"class_short\":\"shorhebekaya\",\"isActive\":true},{\"class_name\":\"Sanabiya Sani\",\"class_short\":\"meskat1\",\"isActive\":true},{\"class_name\":\"Hifz\",\"class_short\":\"hifz\",\"isActive\":true},{\"class_name\":\"Pre Hifz\",\"class_short\":\"pre_hifz\",\"isActive\":true},{\"class_name\":\"Fozilat\",\"class_short\":\"meskat2\",\"isActive\":true}]",
+      //     "created": "2024-12-30 15:02:42"
+      // }) 
+      // data.push({
+      //     "id": 6,
+      //     "type": 2,
+      //     "title": " call test",
+      //     "start_time": "12:30",
+      //     "end_time": "12:40",
       //     "classes": "[{\"class_name\":\"Play\",\"class_short\":\"play\",\"isActive\":true},{\"class_name\":\"Nursery\",\"class_short\":\"nursery\",\"isActive\":true},{\"class_name\":\"KG\",\"class_short\":\"kg\",\"isActive\":true},{\"class_name\":\"One/Saffe Awal\",\"class_short\":\"one\",\"isActive\":true},{\"class_name\":\"Two/Saffe Sani\",\"class_short\":\"two\",\"isActive\":true},{\"class_name\":\"Three/Saffe Sales\",\"class_short\":\"three\",\"isActive\":true},{\"class_name\":\"Four/Saffe Rabe\",\"class_short\":\"four\",\"isActive\":true},{\"class_name\":\"Ibtedaiyah\",\"class_short\":\"five\",\"isActive\":true},{\"class_name\":\"Mutawassitah Awal / Mizan\",\"class_short\":\"mizan\",\"isActive\":true},{\"class_name\":\"Mutawassitah Sani / Nahbemir\",\"class_short\":\"nahbemir\",\"isActive\":true},{\"class_name\":\"Mutawassitah Sales\",\"class_short\":\"kuduri\",\"isActive\":true},{\"class_name\":\"Sanabiya Awal/Shorhebekaya\",\"class_short\":\"shorhebekaya\",\"isActive\":true},{\"class_name\":\"Sanabiya Sani\",\"class_short\":\"meskat1\",\"isActive\":true},{\"class_name\":\"Hifz\",\"class_short\":\"hifz\",\"isActive\":true},{\"class_name\":\"Pre Hifz\",\"class_short\":\"pre_hifz\",\"isActive\":true},{\"class_name\":\"Fozilat\",\"class_short\":\"meskat2\",\"isActive\":true}]",
       //     "created": "2024-12-30 15:02:42"
       // }) 
