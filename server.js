220f29ecd92f0fc3edd4496b4e3f4a8a711c6460
Al-Mamun <mamun@leapinglogic.com>
2025-01-11 13:07:08 +0600

    db added in root folder

--- server.js
+++ server.js
@@ -162,6 +162,6 @@ app.get(`/${WEB_ROUTE}`, (req, res) => {
  
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`); 
+  console.log(`Server is running on http://localhost:${PORT}/app/#`); 
 });
 
