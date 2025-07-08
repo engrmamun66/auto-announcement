ce8ef938ff45d20b240de0fa999e074434c2980d
Al-Mamun <mamun@leapinglogic.com>
2025-07-08 22:19:39 +0600

    Update

--- open.example.bat
+++ open.example.bat
@@ -1,6 +1,9 @@
+@REM ========= For Windows =====================
 @echo off
 @REM start http://localhost:2323/app/#/
+@REM start chrome http://localhost:2323/app/#/
 start microsoft-edge:http://localhost:2323/app/#/
 pm2 start
 @REM timeout /t 2 >nul
 exit
+ 
