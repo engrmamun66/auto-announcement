5717a0b971f08ba2f3e4ee18d14f1d2db7679d98
Al-Mamun <mamun@leapinglogic.com>
2025-07-20 14:44:31 +0600

    Working...

--- front-end/src/components/ReceiverSide.vue
+++ front-end/src/components/ReceiverSide.vue
@@ -1,33 +1,38 @@
 <script setup>
 import { io } from 'socket.io-client'; // ✅ import socket.io-client
-import { onMounted } from 'vue';
+import { onMounted, inject, ref } from 'vue';
+const emitter = inject('emitter');
 
 const secretKey = '1234';
+const audioRef = ref(null);
 const socket = io('http://localhost:2400');
 onMounted(() => {
     console.warn('Receiver Side');
-  socket.emit('join-call', { secretKey, role: 'receiver' });
+    socket.emit('join-call', { secretKey, role: 'receiver' });
 
-  const audio = new Audio();
-  audio.autoplay = true;
+    const audio = audioRef.value
+    audio.autoplay = true;
 
-  const mediaSource = new MediaSource();
-  audio.src = URL.createObjectURL(mediaSource);
+    const mediaSource = new MediaSource();
+    audio.src = URL.createObjectURL(mediaSource);
 
-  mediaSource.addEventListener("sourceopen", () => {
-    const sourceBuffer = mediaSource.addSourceBuffer("audio/webm; codecs=opus");
+    mediaSource.addEventListener("sourceopen", () => {
+        const sourceBuffer = mediaSource.addSourceBuffer("audio/webm; codecs=opus"); 
 
-    socket.on("audio-play", (chunk) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        sourceBuffer.appendBuffer(new Uint8Array(reader.result));
-      };
-      reader.readAsArrayBuffer(chunk);
+        socket.on("audio-play", (chunk) => {
+            if (sourceBuffer && !sourceBuffer.updating) {
+                try {
+                // ✅ Directly append chunk if it's already binary (ArrayBuffer/Uint8Array)
+                sourceBuffer.appendBuffer(new Uint8Array(chunk));
+                } catch (err) {
+                console.error("appendBuffer error", err);
+                }
+            }
+        });
     });
-  });
 
   socket.on("call-ended", () => {
-    alert("Call ended.");
+    emitter.emit('toaster-error', { message: 'Call Ended'})
   });
 });
 </script>
