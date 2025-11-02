8278c657e88b6a8688bd14d14b58cce731440307
Al-Mamun <mamun@leapinglogic.com>
2025-11-02 07:44:31 +0600

    sadf

--- front-end/src/components/modal.vue
+++ front-end/src/components/modal.vue
@@ -30,7 +30,8 @@ function close(){
 
 <template> 
     <Teleport to="body" >
-        <div class="an-modal" v-if="modelValue" @click.stop="close()">
+        <!-- <div class="an-modal" v-if="modelValue" @click.stop="close()"> -->
+        <div class="an-modal" v-if="modelValue" @click.stop="false">
             <div class="modal__content fadeUp" @click.stop="false">
                 <slot name="title">
                     <h3 v-if="title">{{title}}</h3> 
