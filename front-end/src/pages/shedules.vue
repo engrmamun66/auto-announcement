a11ab747b091c7b4d133247dfb3146556bd3b6d3
Al-Mamun <mamun@leapinglogic.com>
2026-01-07 22:34:44 +0600

    df

--- front-end/src/pages/shedules.vue
+++ front-end/src/pages/shedules.vue
@@ -209,7 +209,7 @@ function deleteSchedule(id, i, type=1){
 <template>
     <div>
         <div class="d-flex justify-content-between align-items-center flex-wrap">
-           <h1>{{ !addUpdateMode ? `${tab == 1 ? 'Punch' : 'Call'} Schedules` : (payload.id ? 'Update Schedule' : 'Add Schedule')}}</h1> 
+           <h1>{{ !addUpdateMode ? `${tab == 1 ? 'Punch Schedules' : 'Call Schedules'}` : (payload.id ? 'Update Schedule' : 'Add Schedule')}}</h1> 
        
            <div class="d-flex justify-content-end">
                <Btn v-if="!addUpdateMode" class="me-2" @click="addUpdateMode = true" ><i class='bx bx-plus'></i> Add Schedule</Btn>
