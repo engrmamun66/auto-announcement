592761c023a156b7c3a8822b317f295ccb4daef4
Al-Mamun <mamun@leapinglogic.com>
2026-01-18 15:22:04 +0600

    Update

--- front-end/src/pages/contactus.vue
+++ front-end/src/pages/contactus.vue
@@ -178,6 +178,7 @@ let WhatsAppNumbers = ref([
               <i class="bi bi-phone-fill" style="color: #e2136e"></i>
               <div><strong>Nagad:</strong> 01951259460</div>
             </div>
+
           </div>
         </div>
       </div>
@@ -191,11 +192,11 @@ let WhatsAppNumbers = ref([
     </div>
   </div>
 
-  <iframe
+  <iframe 
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.871814090255!2d90.3519066!3d23.8227541!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c1dfeb618507:0x7d23bc082ebe9348!2sSOFT%20PRO%20IT!5e0!3m2!1sen!2sbd!4v1700000000000"
     width="100%"
     height="450"
-    style="border: 0"
+    style="border:0;border-radius: 12px;"
     loading="lazy"
   >
   </iframe>
