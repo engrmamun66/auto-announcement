7c57a62afce432ed71cf715d54426bdacbc0eba6
Al-Mamun <mamun@leapinglogic.com>
2026-01-19 11:59:03 +0600

    ff

--- front-end/src/pages/contactus.vue
+++ front-end/src/pages/contactus.vue
@@ -340,7 +340,7 @@ body {
 .whatsapp-card {
     width: 100%;
     height: 120px;
-    background: linear-gradient(135deg, #25D366 0%, #128C7E 100%);
+    background: linear-gradient(135deg, #2aa597 0%, #25D366 50%, #128C7E 100%);
     border-radius: 12px;
     padding: 0 30px;
     display: flex;
