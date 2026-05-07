eded8ad6b80c99fa4656296f62764b1ca6784bdb
Al-Mamun <mamun@leapinglogic.com>
2026-05-07 23:47:24 +0600

    df

--- src/zipper/zip-names.js
+++ src/zipper/zip-names.js
@@ -1,6 +1,9 @@
+const { CODE_NUMBER } = global.config.env;
+
 const ZIP_LATEST = 'calling-bird-latest.zip';
 const ZIP_TEMP   = 'calling-bird-latest-debug-temp.zip';
-const ZIP_NEW_SETUP   = 'calling-bird-new-setup-fullcode.zip';
+const ZIP_NEW_SETUP   = `${CODE_NUMBER}__calling-bird-fullcode.zip`;
+
 
 module.exports = { ZIP_LATEST, ZIP_TEMP, ZIP_NEW_SETUP };
 
