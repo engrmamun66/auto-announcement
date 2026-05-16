2eaf9ca6805c7665f151a893a1c8df9dbe624070
Al-Mamun <mamun@leapinglogic.com>
2026-05-16 10:57:29 +0600

    udapte port

--- config.example.js
+++ config.example.js
@@ -48,7 +48,7 @@ module.exports = {
           // #Front End variables
           BASE_URL: `http://localhost:${port_number}`,
           API_BASE_URL: `http://localhost:${port_number}/api`,
-          DEVICE_API_BASE_URL: 'http://127.0.0.1:2323/',
+          DEVICE_API_BASE_URL: 'http://127.0.0.1:8081/',
 
      },
 
