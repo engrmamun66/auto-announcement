cc6038bfaf9e05a57377182dc717bdf0afa38050
Al-Mamun <mamun@leapinglogic.com>
2026-05-20 15:51:45 +0600

    update

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -8,6 +8,9 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  optimizeDeps: {
+    include: ['highcharts', 'highcharts-vue'],
+  },
   envDir: path.resolve(__dirname, '../'),
   resolve: {
     alias: {
