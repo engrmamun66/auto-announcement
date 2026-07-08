c6dd4ed9ae2f161db24a79076c192046372c6927
Al-Mamun <mamun@leapinglogic.com>
2026-07-08 20:26:45 +0600

    dd

--- src/utils.js
+++ src/utils.js
@@ -33,7 +33,7 @@ function getDayMoment(time = '') {
 
 function formatTimeWithPeriod(timeString) {
   const period = getDayMoment(timeString);
-  const time = moment(timeString, ['HH:mm:ss']).locale('bn').format('hh:mm');
+  const time = moment(timeString, ['HH:mm:ss']).locale('bn').format('hh:mm A');
   // return `${period} - ${time}`;
   return `${time}`;
 }
